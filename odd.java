e74aeebd8a4b0f7f64ee8e817f187c9fc5a0d0b2
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-08-02 20:33:30 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class odd
+{
+	public static void main(String[] args)
+	{
+	Scanner input=new Scanner(System.in);
+	int a,b,i;
+	a=input.nextInt();
+	b=input.nextInt();
+	System.out.print(" the range from \n"+a+"to"+b);
+	if(a%2==0)
+	{
+	for(i=a+1;i<=b;i+=2)
+	 
+	System.out.println(i);
+	}
+	else 
+	{
+	for(i=a;i<=b;i+=2)
+
+	System.out.println(i);
+	}
+	}
+}
+	
