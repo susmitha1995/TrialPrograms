4cfd54bc1843639413be0241cf989a9f112d78f3
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-08-02 18:03:04 +0530

    Add files via upload

---
+++ hcf.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class hcf
+{
+    public static void main(String Args[])
+    {
+        System.out.println("Enter 2 numbers");
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+	        int h=1;
+        int p=m*n;
+        for(int i=2;i<p;i++)
+        {
+            if((m%i==0)&&(n%i==0))
+            {
+                h=i;
+	    }
+        }
+        System.out.println("HCF="+h);
+    }
+}
+
+
