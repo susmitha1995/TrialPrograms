31d89f7ea352fbdfbaad22af23ff0c18cfcc943c
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-08-16 21:26:52 +0530

    Add files via upload

---
+++ round.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANJALI
+ */
+public class JavaApplication21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float i;
+        Scanner input=new Scanner(System.in);
+        i=input.nextFloat();
+        System.out.println(java.lang.Math.round(i));
+        System.out.println(java.lang.Math.floor(i));
+    }
+    
+}
