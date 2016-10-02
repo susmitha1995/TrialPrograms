9f7d78cfbf906cd4e0b804b284a0ed022e6eafe0
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-10-02 06:23:08 +0530

    Add files via upload

---
+++ spacecnt.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in=new Scanner(System.in);
+        String str;
+        str=in.nextLine();
+        String[] str1=str.split(" ");
+       System.out.println(str+".  "+"the space present is"+(str1.length-1));
+       System.out.print(str+".   "+"the reduced space is"+(str1.length-2));
+       
+            
+       
+    }
