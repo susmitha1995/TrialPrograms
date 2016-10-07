2f38483a3eded8646ecf3ac2d4cfe1b70cc4f267
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-10-07 19:43:00 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,9 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        String str="hello.jpeg";
+        int n=str.indexOf(".");
+        //System.out.print(n);
+        System.out.print(str.substring(n+1));
+        
+    }
+    
