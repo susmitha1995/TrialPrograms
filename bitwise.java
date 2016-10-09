c658646e6508182eca8d9712ced7107808604d7e
susmitha1995 <susmitha1995@users.noreply.github.com>
2016-10-09 07:44:55 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        int x=10,y=20;
+        x=x^y;
+        y=x^y;
+        x=x^y;
+        System.out.print(x+" ");
+        System.out.print(y);
+    }
+    
